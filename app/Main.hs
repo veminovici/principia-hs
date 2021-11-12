e378f10dabf2980049e127025bc2078c75b6f486
veminovici <veminovici@hotmail.com>
2021-11-12 13:17:49 +0200

    A little bit more chains

--- app/Main.hs
+++ app/Main.hs
@@ -18,15 +18,24 @@ appendReverseWithCount xs ys = do
     modify (+1)
     pure (rxs ++ rys)
 
+-- | Runs a State workflow starting from an initial state.
+runWorkflow :: s -> State s a -> (s, a)
+runWorkflow z (State wf) = 
+    wf z
+
 main :: IO ()
 main = do
     putStrLn "Principia Haskell";
 
-    -- Here is an example for the State monad
+    -- An xample for my State monad
     putStrLn "State monad";
+
     let xs = [1..5];
     let ys = [1..10];
-    let (State sxy) = appendReverseWithCount xs ys;
-    let (count, res) = sxy 0;
+
+    -- Build the workflow (appendReverseWithCount) and then run it
+    -- starting with the initial state set to 0.
+    let (count, res) = runWorkflow 0 $ appendReverseWithCount xs ys;
+
     putStrLn $ "ended - count=" ++ show count ++ ", res=" ++ show res
 
